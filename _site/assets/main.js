7ffb4a833d6b0e031ce0d5d1cd72f8a132fe5c5d
Vita Lampietti <vlampietti@gmail.com>
2017-10-30 16:07:47 -0400

    Updating main.js

---
+++ _site/assets/main.js
@@ -0,0 +1,10 @@
+$('.site-nav').hide();
+$('.front-image').hide();
+$('.wrapper').hide();
+
+
+$(document).ready(() => {
+    $('.site-nav').fadeIn(4000);
+    $('.front-image').fadeIn(4000);
+    $('.wrapper').fadeIn(4000);
+  });
